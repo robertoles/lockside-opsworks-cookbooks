1be1d172572828cd38eba1a5466e6fd4465e5659
Robert Oles <robertoles@me.com>
2014-06-09 14:22:48 +0100

    adding secrets.yml recipe

---
+++ custom_rails/recipes/secrets.rb
@@ -0,0 +1,18 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    source "secrets.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:secrets => deploy[:secrets], :environment => deploy[:rails_env])
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      deploy[:secrets].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+end
